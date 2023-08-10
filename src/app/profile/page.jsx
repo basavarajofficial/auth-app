86ce60effb6d7c8895de95aa232b376115e46dc5
Basavaraj Mannangi <bmannangi34@gmail.com>
2023-08-10 22:22:53 +0530

    profile id

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,34 +1,49 @@
-"use client"
+"use client";
 
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const ProfilePage = () => {
-
   const router = useRouter();
 
+  const [data, setData] = useState("Nothing");
+
   const logout = async () => {
-      try {
-        const response = await axios.get("api/users/logout");
-        console.log(response);
+    try {
+      const response = await axios.get("api/users/logout");
+      console.log(response);
 
-        router.push("/login");
-        
-      } catch (error) {
-        console.log(error.message);
-      }
-  }
+      router.push("/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
 
   return (
-    <div className='flex flex-col justify-center items-center min-h-screen text-white py-4 gap-3'>
+    <div className="flex flex-col justify-center items-center min-h-screen text-white py-4 gap-3">
       <h1>ProfilePage</h1>
+      <br />
 
-      <button className='bg-blue-500 text-xl p-2 rounded-md' onClick={logout}  >Logout</button>
+      <h1>{data === "Nothing" ? "Nothing" :
+        <Link href={`/profile/${data}`} >click here</Link>}</h1>
+
+      <button className="bg-blue-500 text-xl p-2 rounded-md" onClick={logout}>
+        Logout
+      </button>
+
+      <button className="bg-orange-500 text-xl p-2 rounded-md" onClick={getUserDetails}>
+        Your Profile
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
