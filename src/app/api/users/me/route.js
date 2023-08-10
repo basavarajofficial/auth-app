137b62f973d31e9a8f048aadee051de6ff07e1d9
Basavaraj Mannangi <bmannangi34@gmail.com>
2023-08-10 18:35:48 +0530

    logout

---
+++ src/app/api/users/me/route.js
@@ -0,0 +1,23 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server"
+
+
+
+
+export const GET = async (request) => {
+    try {
+        const userID = await getDataFromToken(request);
+
+        const user = await User.findOne({_id: userID}).select( "-password" );
+
+        return NextResponse.json({
+            message: "User Found",
+            data: user
+        })
+        
+    } catch (error) {
+        return NextResponse.json({error: error.message}, {status: 400});
+        
+    }
+}
